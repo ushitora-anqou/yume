48860820f49b71b0b6d5cb73f4895a596f8bb802
Ushitora Anqou <ushitora@anqou.net>
2025-12-25 16:42:50 +0900

    enhance path pattern to allow patterns including colons

--- src/path_pattern.ml
+++ src/path_pattern.ml
@@ -1,4 +1,8 @@
-type single_pattern = L of string | P of string | S
+type single_pattern =
+  | L of string
+  | P of { pattern : string; suffix : string }
+  | S
+
 type t = single_pattern list
 
 let split_on_slash s =
@@ -8,14 +12,24 @@ let of_string (src : string) : t =
   src |> split_on_slash
   |> List.map (function
        | "*" -> S
-       | x when String.starts_with ~prefix:":" x -> P x
+       | x when String.starts_with ~prefix:":" x -> (
+           match String.index_from_opt x 1 ':' with
+           | None -> P { pattern = x; suffix = "" }
+           | Some i ->
+               P
+                 {
+                   pattern = String.sub x 0 i;
+                   suffix = String.sub x i (String.length x - i);
+                 })
        | x -> L x)
 
 let perform ~(pat : t) (src : string) : (string * string) list option =
   let rec aux param = function
     | [], [] | _, [ S ] -> Some param
     | x :: xs, L y :: ys when x = y -> aux param (xs, ys)
-    | x :: xs, P y :: ys -> aux ((y, x) :: param) (xs, ys)
+    | x :: xs, P { pattern; suffix } :: ys when String.ends_with ~suffix x ->
+        let matched = String.sub x 0 (String.length x - String.length suffix) in
+        if matched = "" then None else aux ((pattern, matched) :: param) (xs, ys)
     | _ -> None
   in
   aux [] (split_on_slash src, pat)
